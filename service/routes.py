d387ad7e9c3fce4a6a5501b8f959fe4408faa8dc
HowardPeter <{phuocnt1611@gmail.com}>
2024-06-03 07:52:25 -0400

    nosetests read account past 94%

--- service/routes.py
+++ service/routes.py
@@ -67,8 +67,36 @@ def create_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+# nosetests failed
+# def get_accounts(account_id):
+#         """
+#         Reads an Account
+#         This endpoint will read an Account based the account_id that is requested
+#         """
+#         app.logger.info("Request to read an Account with id: %s", account_id)
+#         account = Account.find(account_id)
+#         if not account:
+#             abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+#         return account.serialize(), status.HTTP_200_OK
 
-# ... place you code here to READ an account ...
+# nosetests past
+def get_accounts(account_id):
+    """
+    Reads an Account
+    This endpoint will read an Account based the account_id that is requested
+    """
+    app.logger.info("Request to read an Account with id: %s", account_id)
+
+    # Find the account
+    account = Account.find(account_id)
+
+    # Handle case where account is not found
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, description=f"Account with id {account_id} not found")
+
+    # Return the serialized account with a status code of 200 OK
+    return jsonify(account.serialize()), status.HTTP_200_OK
 
 
 ######################################################################
