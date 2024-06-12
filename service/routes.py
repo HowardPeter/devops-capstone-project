b2d3ad947067bc87ffd1a3c8776b1b57834c6352
HowardPeter <phuocnt1611@gmail.com>
2024-06-12 07:16:23 -0400

    fix lint with flake8

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,8 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 @app.route("/accounts", methods=["GET"])
+
+
 def list_accounts():
     """
     List all Accounts
@@ -77,19 +79,6 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["GET"])
-# nosetests failed
-# def get_accounts(account_id):
-#         """
-#         Reads an Account
-#         This endpoint will read an Account based the account_id that is requested
-#         """
-#         app.logger.info("Request to read an Account with id: %s", account_id)
-#         account = Account.find(account_id)
-#         if not account:
-#             abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-#         return account.serialize(), status.HTTP_200_OK
-
-# nosetests past
 def get_accounts(account_id):
     """
     Reads an Account
@@ -106,7 +95,6 @@ def get_accounts(account_id):
 
     # Return the serialized account with a status code of 200 OK
     return jsonify(account.serialize()), status.HTTP_200_OK
-
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -144,8 +132,6 @@ def delete_accounts(account_id):
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
